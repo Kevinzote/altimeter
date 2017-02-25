d15e8eee352f2d0d99f5acf39bc899e5eb83d7ae
mlosa010 <mlosa010@fiu.edu>
2017-02-24 20:44:04 -0500

    first altimeter uplod

---
+++ Altimeter3/Altimeter3.ino
@@ -0,0 +1,13 @@
+#include <Adafruit_BMP085.h>
+
+// this is the first push to git make sure to download the adafuit library for pressure sensor
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
